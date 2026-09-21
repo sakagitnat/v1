9affd440207d08354fe9a4cd6e5ba7c287b21605
sakagitnat <sakagitnat@gmail.com>
2026-09-21 09:52:58 +0700

    Include volatility expansion in autonomous research lab

--- scripts/research_cfd_strategies.py
+++ scripts/research_cfd_strategies.py
@@ -68,12 +68,14 @@ from trading.cfd.research_lab import evaluate_candidate, generate_candidate_para
 from trading.cfd.strategy import EmaCrossoverStrategy
 from trading.cfd.strategy_registry import list_all
 from trading.cfd.support_resistance import SupportResistanceReversionStrategy
+from trading.cfd.volatility_expansion import VolatilityExpansionBreakoutStrategy
 from trading.config import settings
 
 from optimize_cfd_breakout import PARAM_GRID as BREAKOUT_PARAM_GRID
 from optimize_cfd_mean_reversion import PARAM_GRID as MEAN_REVERSION_PARAM_GRID
 from optimize_cfd_rsi_reversion import PARAM_GRID as RSI_REVERSION_PARAM_GRID
 from optimize_cfd_support_resistance import PARAM_GRID as SUPPORT_RESISTANCE_PARAM_GRID
+from research_cfd_volatility_expansion import PARAM_GRID as VOLATILITY_EXPANSION_PARAM_GRID
 from optimize_cfd_strategy import (
     MAX_DRAWDOWN_CAP,
     MIN_TRADES,
@@ -127,6 +129,13 @@ STRATEGY_SPECS = [
         "param_grid": SUPPORT_RESISTANCE_PARAM_GRID,
         "filter_fn": None,
         "suited_regimes": ["ranging"],
+    },
+    {
+        "name": "volatility_expansion",
+        "cls": VolatilityExpansionBreakoutStrategy,
+        "param_grid": VOLATILITY_EXPANSION_PARAM_GRID,
+        "filter_fn": None,
+        "suited_regimes": ["ranging", "trending"],
     },
 ]
 
